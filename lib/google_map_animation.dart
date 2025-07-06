2dfb33cfb606dd7d2d076d8ed27e135d5f66262d
relani <manishrelani4598@gmail.com>
2025-07-06 14:04:29 +0530

    added fadin_animator

--- lib/google_map_animation.dart
+++ lib/google_map_animation.dart
@@ -1,4 +1,5 @@
 export 'src/animator/color_animator.dart';
+export 'src/animator/fade_in_progressive_animator.dart';
 export 'src/animator/snack_animator.dart';
 export 'src/core/controllers/map_animation_controller.dart';
 export 'src/core/model/animated_polyline.dart';
---
+++ lib/src/animator/fade_in_progressive_animator.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import '../../google_map_animation.dart';
+
+class FadeInProgressiveAnimator extends PolylineAnimator {
+  const FadeInProgressiveAnimator({
+    super.duration = const Duration(seconds: 5),
+    super.curve = Curves.ease,
+    super.repeat = false,
+  });
+
+  @override
+  Polyline animate(Polyline polyline, double progress) {
+    if (progress <= 0.3) {
+      return polyline.copyWith(colorParam: polyline.color.withValues(alpha: progress / 0.3));
+    } else {
+      final animationProgress = (progress - 0.3) / 0.7;
+      final startIndex = (polyline.points.length * animationProgress).round();
+      final points = polyline.points.skip(startIndex).toList();
+      return polyline.copyWith(pointsParam: points);
+    }
+  }
+}
